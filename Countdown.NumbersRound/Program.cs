a6c430b0a135d7f5b5c0a24227898065ac1421e8
David Francis <dm.francis@live.co.uk>
2019-06-18 19:56:54 +0100

    Add project files.

---
+++ Countdown.NumbersRound/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Countdown.NumbersRound
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
