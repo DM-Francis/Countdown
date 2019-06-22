b715414a8df9edde836938029f872b729d5aa428
David Francis <dm.francis@live.co.uk>
2019-06-22 17:12:09 +0100

    Added stuff

---
+++ Countdown.NumbersRound/Operation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Countdown.NumbersRound
+{
+    public enum Operation
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
+    }
+}
--- Countdown.NumbersRound/Program.cs
+++ Countdown.NumbersRound/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Combinatorics.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Countdown.NumbersRound
 {
@@ -6,7 +9,47 @@ namespace Countdown.NumbersRound
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Objectives:
+            //  - Get all possible combinations of n numbers, for n in [1,2,3,4,5,6]
+            //  - Get all possible combinations of the 4 operations. +, -, /, *
+            //  - Get all possibilities for placing brackets around n numbers. - Hardest.
+
+            // Numbers
+            var all3Variations = CreateNumberVariations(3);
+
+            Console.WriteLine(all3Variations.Count);
+
+            foreach(var variant in all3Variations)
+            {
+                Console.WriteLine(string.Join(',', variant));
+            }
+
+            // Operations
+            var operator3Variations = CreateOperationVariations(3);
+
+            foreach(var variant in operator3Variations)
+            {
+                Console.WriteLine(string.Join(',', variant));
+            }
+        }
+
+        private static Variations<int> CreateNumberVariations(int n)
+        {
+            if (n < 1 || n > 6)
+            {
+                throw new ArgumentOutOfRangeException(nameof(n), n, $"{nameof(n)} must be between 1 and 6.");
+            }
+
+            var domain = new List<int> { 1, 2, 3, 4, 5, 6 };
+
+            return new Variations<int>(domain, n, GenerateOption.WithRepetition);
+        }
+
+        private static Variations<Operation> CreateOperationVariations(int n)
+        {
+            var domain = new List<Operation> { Operation.Addition, Operation.Subtraction, Operation.Multiplication, Operation.Division };
+
+            return new Variations<Operation>(domain, n, GenerateOption.WithRepetition);
         }
     }
 }
