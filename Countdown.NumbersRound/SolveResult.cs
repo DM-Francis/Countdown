8a5b33b8349d2aeb61ad64b078bdc59caa5c674a
David Francis <dm.francis@live.co.uk>
2019-07-09 22:08:43 +0100

    Add ability to find close results if no solutions are found and show an appropiate message in the client.

---
+++ Countdown.NumbersRound/SolveResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Countdown.NumbersRound
+{
+    public class SolveResult
+    {
+        public int ClosestDiff { get; set; }
+        public List<string> Solutions { get; set; }
+    }
+}
