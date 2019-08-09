935776d3ea616b0e17a7e0ccee9bc24e3bb8a2e3
David Francis <dm.francis@live.co.uk>
2019-08-09 23:22:03 +0100

    Misc change to PrefixExpression.cs

--- Countdown.NumbersRound/PrefixNotationBased/PrefixExpression.cs
+++ Countdown.NumbersRound/PrefixNotationBased/PrefixExpression.cs
@@ -9,16 +9,12 @@ namespace Countdown.NumbersRound.PrefixNotationBased
     {
         public static List<string> Operations = new List<string> { "+", "-", "/", "*" };
 
-        private Stack<string> _stack;
-        private int _numsSinceTopOperation;
-        private int _operationsInStack;
-
         public IReadOnlyList<string> RawExpression { get; }
 
         public PrefixExpression(List<string> expression)
         {
             RawExpression = expression;
-            if (!IsValid()) throw new FormatException();
+            //if (!IsValid()) throw new FormatException();
         }
 
         public PrefixExpression(string constant)
@@ -28,32 +24,36 @@ namespace Countdown.NumbersRound.PrefixNotationBased
 
         public float Evaluate()
         {
-            var tokenStack = new Stack<string>();
+            var tokenStack = new Stack<float>();
             foreach(var token in RawExpression.Reverse())
             {
-                float num;
-                if (float.TryParse(token, out num))
+                if (float.TryParse(token, out float num))
                 {
-                    tokenStack.Push(num.ToString());
+                    tokenStack.Push(num);
                 }
                 else
                 {
-                    float num1 = float.Parse(tokenStack.Pop());
-                    float num2 = float.Parse(tokenStack.Pop());
+                    float num1 = tokenStack.Pop();
+                    float num2 = tokenStack.Pop();
+
+                    if (!OperationIsValid(token, num1, num2))
+                    {
+                        return float.NaN;
+                    }
 
                     switch (token)
                     {
                         case "+":
-                            tokenStack.Push((num1 + num2).ToString());
+                            tokenStack.Push(num1 + num2);
                             break;
                         case "-":
-                            tokenStack.Push((num1 - num2).ToString());
+                            tokenStack.Push(num1 - num2);
                             break;
                         case "/":
-                            tokenStack.Push((num1 / num2).ToString());
+                            tokenStack.Push(num1 / num2);
                             break;
                         case "*":
-                            tokenStack.Push((num1 * num2).ToString());
+                            tokenStack.Push(num1 * num2);
                             break;
                         default:
                             throw new InvalidOperationException($"Expected a string representing an operation, but instead got {token}.");
@@ -66,7 +66,7 @@ namespace Countdown.NumbersRound.PrefixNotationBased
                 throw new InvalidOperationException($"Expected only 1 remaining number in the stack, but there were {tokenStack.Count}");
             }
 
-            return float.Parse(tokenStack.Pop());
+            return tokenStack.Pop();
         }
 
         private bool IsNumber(string s)
@@ -93,6 +93,47 @@ namespace Countdown.NumbersRound.PrefixNotationBased
                 string last = RawExpression.Last();
                 return IsOperation(first) && IsNumber(last);
             }
+        }
+
+        private bool OperationIsValid(string operation, float num1, float num2)
+        {
+            if (num1 < num2)
+            {
+                return false;
+            }
+
+            if (operation == "*" && (num1 == 1 || num2 == 1))
+            {
+                return false;
+            }
+            else if (operation == "/")
+            {
+                if (num2 == 1)
+                {
+                    return false;
+                }
+                else if (num1 % num2 != 0)
+                {
+                    return false;
+                }
+                else if (num2 * num2 == num1)
+                {
+                    return false;
+                }
+            }
+            else if (operation == "-")
+            {
+                if (num1 == num2)
+                {
+                    return false;
+                }
+                else if (num1 == 2 * num2)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
