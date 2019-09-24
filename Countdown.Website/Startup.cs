fbe96526be57999dab1cd049d62f5a0c6252ae01
David Francis <dm.francis@live.co.uk>
2019-09-24 21:14:08 +0100

    Remove unnecessary usings

--- Countdown.Website/Startup.cs
+++ Countdown.Website/Startup.cs
@@ -1,7 +1,6 @@
 using Countdown.NumbersRound.Solve;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
