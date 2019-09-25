db8ea87a92d876fb3de5ebed892eff903e3a1c24
David Francis <dm.francis@live.co.uk>
2019-09-25 23:16:37 +0100

    Aspnetcore 3.0 (#7)
    * Migration to ASP.NET Core 3.0

--- Countdown.Website/Program.cs
+++ Countdown.Website/Program.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Countdown.Website
@@ -8,12 +8,15 @@ namespace Countdown.Website
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .ConfigureLogging(logging => logging.AddAzureWebAppDiagnostics())
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureLogging(logging => logging.AddAzureWebAppDiagnostics());
+                });
     }
 }
--- Countdown.Website/Startup.cs
+++ Countdown.Website/Startup.cs
@@ -1,10 +1,10 @@
 using Countdown.NumbersRound.Solve;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Countdown.Website
 {
@@ -21,7 +21,6 @@ namespace Countdown.Website
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
                 .AddRazorPagesOptions(options =>
                     options.Conventions
                         .AddPageRoute("/NumbersRound", "/")
@@ -34,12 +33,12 @@ namespace Countdown.Website
             services.AddTransient<ISolver, Solver>();
 
             services.AddSwaggerGen(c =>
-                c.SwaggerDoc("v1", new Info { Title = "Countdown Solve API", Version = "v1" })
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Countdown Solve API", Version = "v1" })
             );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -52,15 +51,20 @@ namespace Countdown.Website
             }
 
             app.UseHttpsRedirection();
+            app.UseStatusCodePages();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
 
             app.UseSwagger();
             app.UseSwaggerUI(c =>
-                c.SwaggerEndpoint("./v1/swagger.json", "Countdown Solve API v1")
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Countdown Solve API v1")
             );
 
-            app.UseMvc();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+            });
         }
     }
 }
